a5752481ebe1bb5fbe075e8fb9455bd9e8dd745e
Gema <gmp395@gmail.com>
2026-07-29 16:30:41 +0200

    Sincroniza App.jsx con el código del live coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
- return <h1>Hola React</h1>;
-}
 
-export default App;
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  const technologies = ["HTML", "CSS", "React"];
+
+  return (
+    <>
+      <h1>Contador: {count}</h1>
+      <ul>
+        {technologies.map((tech) => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <button onClick={() => setCount(count + 1)}>
+        Incrementar
+      </button>
+      {count >= 5 && <p>Has llegado a 5</p>}
+    </>
+  );
+}
+export default App; 
